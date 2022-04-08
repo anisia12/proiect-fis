461e0f16e9ffedcf846e01fe671326cd17e2e569
anisia12 <anisia.rosu@student.upt.ro>
2022-04-08 22:45:27 +0300

    GYM-37 Registration done

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
         initDirectory();
         UserService.initDatabase();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
-        primaryStage.setTitle("Registration Example");
+        primaryStage.setTitle("Create an account");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
